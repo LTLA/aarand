4a4f9242c306536ddb1b7bceef7b098c3a3d5168
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-09-07 23:55:23 -0700

    Added draft of the discrete integer sampler.

--- include/aarand/aarand.hpp
+++ include/aarand/aarand.hpp
@@ -2,12 +2,14 @@
 #define AARAND_AARAND_HPP
 
 #include <cmath>
+#include <limits>
 
 namespace aarand {
 
 /**
  * @tparam T Floating point type.
- * @tparam Engine A random number generator class with `operator()`, `min()` (static) and `max()` (static) methods.
+ * @tparam Engine A random number generator class with `operator()`, `min()` (static) and `max()` (static) methods,
+ * where the `result_type` is an unsigned integer value.
  *
  * @param eng Instance of an RNG class like `std::mt19937_64`.
  *
@@ -15,8 +17,13 @@ namespace aarand {
  */
 template<typename T = double, class Engine>
 T standard_uniform(Engine& eng) {
+    static_assert(!std::numeric_limits<typename Engine::result_type>::is_signed);
+    static_assert(std::numeric_limits<typename Engine::result_type>::is_integer);
+
     // Stolen from Boost, see https://www.boost.org/doc/libs/1_67_0/boost/random/uniform_01.hpp
-    constexpr double factor = 1.0 / (static_cast<T>(Engine::max() - Engine::min()) + static_cast<T>(std::numeric_limits<Engine::result_type>::is_integer));
+    // The +1 probably doesn't matter for 64-bit generators, but is helpful for engines with 
+    // fewer output bits, to reduce the (small) probability of sampling 1's.
+    constexpr double factor = 1.0 / (static_cast<T>(Engine::max() - Engine::min()) + 1.0);
     double result;
     do {
         result = static_cast<T>(eng() - Engine::min()) * factor;
@@ -26,7 +33,8 @@ T standard_uniform(Engine& eng) {
 
 /**
  * @tparam T Floating point type.
- * @tparam Engine A random number generator class with `operator()`, `min()` (static) and `max()` (static) methods.
+ * @tparam Engine A random number generator class with `operator()`, `min()` (static) and `max()` (static) methods,
+ * where the `result_type` is an unsigned integer value.
  *
  * @param eng Instance of an RNG class like `std::mt19937_64`.
  *
@@ -44,7 +52,8 @@ std::pair<T, T> standard_normal(Engine& eng) {
 
 /**
  * @tparam T Floating point type.
- * @tparam Engine A random number generator class with `operator()`, `min()` (static) and `max()` (static) methods.
+ * @tparam Engine A random number generator class with `operator()`, `min()` (static) and `max()` (static) methods,
+ * where the `result_type` is an unsigned integer value.
  *
  * @param eng Instance of an RNG class like `std::mt19937_64`.
  *
@@ -54,6 +63,27 @@ template<typename T = double, class Engine>
 T standard_exponential(Engine& eng) {
     return -std::log(standard_uniform(eng));
 }
+
+template<typename T = int, class Engine>
+T discrete_uniform(Engine& eng, T max) {
+    typedef typename Engine::result_type R;
+    static_assert(std::numeric_limits<R>::is_integer);
+    static_assert(!std::numeric_limits<R>::is_signed);
+
+    constexpr R range = Engine::max() - Engine::min();
+    static_assert(range >= std::numeric_limits<T>::max());
+
+    // Under the assertion above, it is impossible for max == Engine::max() +
+    // 1. So we don't have to deal with the crap about combining draws to get
+    // enough entropy, which is 90% of the Boost implementation.
+    const R limit = range - range % max;
+    T draw;
+    do {
+        draw = (eng() - Engine::min()) % max;
+    } while (draw > limit);
+    return draw;
+}
+
 
 
 
