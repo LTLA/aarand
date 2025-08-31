22093d1bbab80b8c77ccf7d8003b64a82d47ae5c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-08-31 23:49:25 +0000

    Deploying to gh-pages from @ LTLA/aarand@55d9c985a52ef31e083ba2e61c84425bedb8f78c 🚀

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['discrete_5funiform_0',['discrete_uniform',['../namespaceaarand.html#aff274eeaaf82e508866401bad2673c93',1,'aarand']]],
+  ['discrete_5funiform_0',['discrete_uniform',['../namespaceaarand.html#ab90593916ef696067d706328dcbf5bb6',1,'aarand']]],
   ['distributions_20in_20c_1',['Aaron&apos;s random distributions in C++',['../index.html',1,'']]]
 ];
--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['s_20random_20distributions_20in_20c_0',['Aaron&apos;s random distributions in C++',['../index.html',1,'']]],
-  ['sample_1',['sample',['../namespaceaarand.html#afe527ee725e4917acb4d0300e0a06552',1,'aarand::sample(In values, size_t n, size_t s, Out output, Engine &amp;eng)'],['../namespaceaarand.html#ae9b1fe274949d90017d5c027379d1d11',1,'aarand::sample(size_t bound, size_t s, Out output, Engine &amp;eng)']]],
-  ['shuffle_2',['shuffle',['../namespaceaarand.html#a61a6cf8b1e384c5d06a4b33880bbc56d',1,'aarand']]],
-  ['standard_5fexponential_3',['standard_exponential',['../namespaceaarand.html#a351ab484467744259898469df3ef02db',1,'aarand']]],
-  ['standard_5fnormal_4',['standard_normal',['../namespaceaarand.html#a4e7dca20ad7b4cd391cf225276a5d562',1,'aarand']]],
-  ['standard_5funiform_5',['standard_uniform',['../namespaceaarand.html#aad381a939ef57d5759cbbccbb7073b46',1,'aarand']]]
+  ['sample_1',['sample',['../namespaceaarand.html#a114562d198af107fac3a968da5a44862',1,'aarand::sample(InputIterator_ values, const Length_ n, const Length_ s, OutputIterator_ output, Engine_ &amp;eng)'],['../namespaceaarand.html#a47d979d2a1dac483d6e007360587c2a2',1,'aarand::sample(const Length_ bound, const Length_ s, OutputIterator_ output, Engine_ &amp;eng)']]],
+  ['shuffle_2',['shuffle',['../namespaceaarand.html#ae5ed7407c9fb663af00728c3c8cc3dac',1,'aarand']]],
+  ['standard_5fexponential_3',['standard_exponential',['../namespaceaarand.html#a3cf87e0bca25edd84c43d53ed2b01363',1,'aarand']]],
+  ['standard_5fnormal_4',['standard_normal',['../namespaceaarand.html#a8008eebc322824e6edaa308814e4d889',1,'aarand']]],
+  ['standard_5funiform_5',['standard_uniform',['../namespaceaarand.html#add44b1a2b7f0b038d30c0babd0aca66e',1,'aarand']]]
 ];
